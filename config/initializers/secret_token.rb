813abf2244e5bfe4294d8d8334c77259cb7c5528
gjpv <gjpv@linux-5jy8.site>
2013-06-28 00:46:38 -0500

    GP, demo twitter-bootstrap-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '8ad66c2b06830808558653bb3036bf26aa0fb385cbfcd09851d32c79713d85197f06ba014cd8de3602350f59036eef0bbd33c67013691d401912f9fbe26971b9'
